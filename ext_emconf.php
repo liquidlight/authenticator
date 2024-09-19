4c71aa8f37aa3e5db7437bde303b5ae01609ea4d
Mike Street <mikestreety@gmail.com>
2024-09-19 20:14:40 +0100

    build: Expand support to TYPO3 10

--- ext_emconf.php
+++ ext_emconf.php
@@ -26,7 +26,7 @@ $EM_CONF[$_EXTKEY] = [
 	'version'          => '2.0.0',
 	'constraints'      => [
 		'depends' => [
-			'typo3' => '9.0.0-9.9.99',
+			'typo3' => '9.0.0-10.9.99',
 		],
 		'conflicts' => [],
 		'suggests'  => [],
